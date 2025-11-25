d77c0a7e08ab54a07c99870704a7ef913d299105
JayaramDasari <jayaram.dasari@thekaustschool.org>
2025-11-25 21:44:26 +0300

    Day 2
    The first day, I created the physical product and connected it to Arduino IDE. Today I started code on pycharm to gather the data from the IDE and to start processing it. I had to research a lot for this code as it is mostly new to me.

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hello world")
+import serial
+import time
+import matplotlib.pyplot as plt
+
+serial = serial.Serial('/dev/cu.usbmodem1401', 9600, timeout = 1)
+
+time.sleep(2)
+
+times = []
+temps = []
+humidity = []
+
+start_time = time.time()
+
+fig, (ax1, ax2) = plt.subplots (2, 1, fisize = (7,6))
+
+while True:
+        line = serial.readline().decode("utf-8", errors='ignore').strip()
+
+        if not line:
+            continue
+
+
+        try:
+            temp_str, hum_str = line.split(',')
+
+            temp_val = float(temp_str)
+            hum_val = float(hum_str)
+
+        except ValueError:
+            print("Bad line", line)
+            continue
+
+    current_time = time.time() - start_time
