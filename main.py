1d821d4a67dbcfa07cd1d8fa163332e0050fc5d1
JayaramDasari <jayaram.dasari@thekaustschool.org>
2025-11-29 10:58:54 +0300

    Error fixes and buzzer code
    Today I fixed some errors after doing testing using the actual circuit. Also, I wanted to add buzzer code on my python script in pycharm instead of just having it on the Arduino IDE. All though this code doesn't actually control the buzzer, it should show a text in the middle of the graph saying "Buzzer On" once the thresholds are breached. It basically imitates the actual buzzer code in C++ on the Arduino IDE.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import serial
 import time
 import matplotlib.pyplot as plt
 
-serial = serial.Serial('/dev/cu.usbmodem1401', 9600, timeout = 1)
+ser = serial.Serial('/dev/cu.usbmodem1401', 9600, timeout = 1)
 
 time.sleep(2)
 
@@ -12,26 +12,24 @@ hums = []
 
 start_time = time.time()
 
-fig, (ax1, ax2) = plt.subplots (2, 1, fisize = (7,6))
+fig, (ax1, ax2) = plt.subplots (2, 1, figize = (7,6))
 
 while True:
-    line = serial.readline().decode("utf-8", errors='ignore').strip()
+    line = ser.readline().decode("utf-8", errors='ignore').strip()
 
     if not line:
         continue
-
 
     try:
         temp_str, hum_str = line.split(',')
         temp_val = float(temp_str)
         hum_val = float(hum_str)
-
     except ValueError:
         print("Bad line", line)
         continue
 
     current_time = time.time() - start_time
-    time.append(current_time)
+    times.append(current_time)
     temps.append(temp_val)
     hums.append(hum_val)
 
@@ -39,13 +37,37 @@ while True:
     ax2.clear()
 
     ax1.plot(times, temps, label= "Temperature (°C)")
+    ax1.axhline(26, color='red', linestyle='--', label = "Buzzer Threshold (26°C)")
     ax1.set_ylabel("Temperature (°C)")
     ax1.legend()
 
     ax2.plot(times, hums, label = "Humidity (%)", color='orange')
+    ax2.set_axline(65, color = 'red',linestyle='--', label = "Buzzer Threshold")
     ax2.set_ylabel("Humidity (%)")
     ax2.set_xlabel("Time (s)")
     ax2.legend()
+
+    buzzer_on = False
+    if temp_val > 26 or hum_val > 65:
+        buzzer_on = True
+
+    if buzzer_on:
+        ax1.text(
+            0.5 , 0.9, "BUZZER ON",
+            tranform=ax1.transAxes,
+            fontsize=16, color ='red',
+            fontweight='bold',
+            ha='center',
+        )
+
+    if buzzer_on:
+        ax2.text(
+            0.5 , 0.9, "BUZZER ON",
+            transform=ax2.transAxes,
+            fontsize=16, color= 'red',
+            fontweight='bold',
+            ha='center',
+        )
 
     plt.tight_layout()
     plt.pause(0.1)
